6da7ec0103100510fe7ac684aae5822b7ae6d5e0
gnb <itsgarnett@gmail.com>
2024-02-07 21:40:32 +1300

    Partial translation, need to fix quote at end of

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Dictionary-To-JSON-parser
+
 This is a GoLang script to parse the Gutenberg Websters dictionary https://www.gutenberg.org/ebooks/29765
+
+## How to run
+
+At the moment you need Go installed on your device, then the command
+
+```bash
+go run applicaiton.go
+```
+
+will excecute the application
--- application.go
+++ application.go
@@ -5,18 +5,35 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strings"
 )
 
 func main() {
 
 	// Get Dict Data in buffer
 	file, error := os.Open("resources/dict.txt")
-	_DICTIONARY_WORD_REGEX, _ := regexp.Compile("^[A-Z][A-Z0-9\\. -]*$")
-	_DICTIONARY_END_REGEX, _ := regexp.Compile("^\\s*[\\*]{3} END.*$")
+
+	wordRegex, _ := regexp.Compile("^[A-Z][A-Z0-9\\. '-]*$")
+	dictEndRegex, _ := regexp.Compile("^\\s*[\\*]{3} END.*$")
+	dictStartRegex, _ := regexp.Compile("^\\s*[\\*]{3} START.*$")
+	newLineRegex, _ := regexp.Compile("^\\n$")
+
+	const (
+		preStart int = iota
+		beforeFirstWord
+		parsingWord
+		parsingPronounciation
+		parsingDefinitionFirstLine
+		parsingDefinition
+	)
+
+	var phase = preStart
+
 	if error != nil {
 		fmt.Printf("I/O error: %s", error)
 		return
 	}
+
 	// Close file on function exit
 	defer file.Close()
 
@@ -25,23 +42,62 @@ func main() {
 
 	// Iterate over each line
 	var lastWord = "$"
-	fmt.Println("{\n\t")
+
+	i := 0
+
 	for scanner.Scan() {
+
+		if i > 190 {
+			break
+		} else {
+			i++
+		}
+
+		// read new line from buffer
 		line := scanner.Text()
-		if _DICTIONARY_END_REGEX.MatchString(line) {
-			// fmt.Printf(line)
-			fmt.Printf("\tfinished reading\n")
+
+		if phase == preStart && dictStartRegex.MatchString(line) {
+			phase = beforeFirstWord
+			continue
+		} else if phase == preStart {
+			continue
+		}
+
+		if dictEndRegex.MatchString(line) {
+			fmt.Println("\t\"\n}")
+			fmt.Printf("\n\tfinished reading\n")
 			break
 		}
 
-		if _DICTIONARY_WORD_REGEX.MatchString(line) && lastWord != line {
-			fmt.Printf("\"\n},\n{\n\t\"word\": \"%s\",\n\t\"definition\": \"", line)
-			lastWord = line
-		} else {
-			fmt.Printf("\t%s", line)
-		}
-	}
-	fmt.Println("\t\"\n}")
+		if wordRegex.MatchString(line) && lastWord != line {
+			if phase != beforeFirstWord {
+				fmt.Printf("\"]\"\n},\n")
+			}
 
-	// fmt.Println(lastWord)
+			fmt.Printf("{\n\t\"word\": \"%s\",", line)
+			phase = parsingPronounciation
+			lastWord = line
+
+		} else if phase == parsingPronounciation {
+			fmt.Printf("\n\t\"pronounciation\": \"%s\",", strings.Replace(line, "\"", "\\\"", -1))
+			phase = parsingDefinitionFirstLine
+
+		} else if newLineRegex.MatchString(line) {
+
+			if phase == parsingDefinition {
+				fmt.Printf("\"\n")
+			}
+
+			fmt.Printf(" ,")
+
+		} else if phase == parsingDefinitionFirstLine {
+			fmt.Printf("\n\t\"definition\": [\n\t\t\"%s", strings.Replace(line, "\"", "\\\"", -1))
+			phase = parsingDefinition
+
+		} else if phase == parsingDefinition {
+			fmt.Printf("%s", strings.Replace(line, "\"", "\\\"", -1))
+		}
+
+		// fmt.Println("\n\t\t\t", phase)
+	}
 }
