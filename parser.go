3975304a6f599430e7be209ab7d619cd297dccfb
gnb <itsgarnett@gmail.com>
2024-02-11 16:40:49 +1300

    MVP delivered, not tested. Want to make a fork and do a struct approach

--- parser.go
+++ parser.go
@@ -8,25 +8,35 @@ import (
 	"strings"
 )
 
+const (
+	preStart int = iota
+	beforeFirstWord
+	parsingWord
+	parsingPronounciationFirstLine
+	parsingPronounciation
+	parsingDefinitionFirstLine
+	parsingDefinition
+	newLineInDefinition
+)
+
+const (
+	WordRegexStr      = `^[A-Z](?:[A-Z0-9\. ';-]*)$`
+	DictEndRegexStr   = `^\s*[\*]{3} END.*$`
+	DictStartRegexStr = `^\s*[\*]{3} START.*$`
+	NewLineRegexStr   = "^$"
+)
+
+var (
+	WordRegex      = regexp.MustCompile(WordRegexStr)
+	DictEndRegex   = regexp.MustCompile(DictEndRegexStr)
+	DictStartRegex = regexp.MustCompile(DictStartRegexStr)
+	NewLineRegex   = regexp.MustCompile(NewLineRegexStr)
+)
+
 func main() {
 
 	// Get Dict Data in buffer
 	file, error := os.Open("resources/dict.txt")
-
-	wordRegex := regexp.MustCompile(`^[A-Z](?:[A-Z0-9\. ';-]*)$`)
-	dictEndRegex := regexp.MustCompile(`^\s*[\*]{3} END.*$`)
-	dictStartRegex := regexp.MustCompile(`^\s*[\*]{3} START.*$`)
-	newLineRegex := regexp.MustCompile("^$")
-
-	const (
-		preStart int = iota
-		beforeFirstWord
-		parsingWord
-		parsingPronounciation
-		parsingDefinitionFirstLine
-		parsingDefinition
-		newLineInDefinition
-	)
 
 	var phase = preStart
 
@@ -42,72 +52,79 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	// Iterate over each line
-	i := 0
+	// i := 0
 
 	for scanner.Scan() {
 
-		if i > 190 {
-			break
-		} else {
-			i++
-		}
+		// if i > 290 {
+		// 	break
+		// } else {
+		// 	i++
+		// }
 
-		// read new line from buffer
-		line := scanner.Text()
-
-		if phase == preStart && dictStartRegex.MatchString(line) {
-			phase = beforeFirstWord
-			continue
-		} else if phase == preStart {
-			continue
-		}
-
-		if dictEndRegex.MatchString(line) {
-			fmt.Println("\t\"\n}")
-			fmt.Printf("\n\tfinished reading\n")
+		// if phase == parsingDefinition {
+		// }
+		if ParseDict(&phase, scanner.Text()) == 0 {
 			break
 		}
 
-		if wordRegex.MatchString(line) {
-			if phase == newLineInDefinition {
-				fmt.Printf("\n\t]")
-			} else if phase != beforeFirstWord {
-				fmt.Printf("]\"\n},\n")
-			}
-
-			fmt.Printf("\n},\n{\n\t\"word\": \"%s\",", line)
-			phase = parsingPronounciation
-
-		} else if phase == parsingPronounciation {
-			fmt.Printf("\n\t"+`"pronounciation": "%s",`, strings.Replace(line, `"`, `\"`, -1))
-			phase = parsingDefinitionFirstLine
-
-		} else if newLineRegex.MatchString(line) {
-
-			if phase == parsingDefinition {
-				phase = newLineInDefinition
-				fmt.Printf(`"`)
-			}
-			// if phase == parsingDefinition {
-			// }
-
-		} else if phase == parsingDefinitionFirstLine {
-			fmt.Printf(
-				"\n\t\"definition\": [\n\t\"%s",
-				strings.Replace(line, `"`, `\"`, -1))
-
-			phase = parsingDefinition
-
-		} else if phase == parsingDefinition || phase == newLineInDefinition {
-
-			if phase == newLineInDefinition {
-				fmt.Printf(",\n\t\"")
-				phase = parsingDefinition
-			}
-
-			fmt.Printf("%s", strings.Replace(line, `"`, `\"`, -1))
-		}
-
-		// fmt.Println("\n\t\t\t", phase)
 	}
+}
+
+func ParseDict(phase *int, line string) int {
+	if *phase == preStart && DictStartRegex.MatchString(line) {
+		*phase = beforeFirstWord
+		return 1
+	} else if *phase == preStart {
+		return 1
+	}
+
+	if DictEndRegex.MatchString(line) {
+		fmt.Println("\t\"\n}")
+		fmt.Printf("\n\tfinished reading\n")
+		return 0
+	}
+
+	if NewLineRegex.MatchString(line) {
+
+		if *phase == parsingDefinition {
+			*phase = newLineInDefinition
+			fmt.Printf(`"`)
+		} else if *phase == parsingPronounciation {
+			*phase = parsingDefinitionFirstLine
+			fmt.Printf(`",`)
+		}
+
+	} else if WordRegex.MatchString(line) {
+		if *phase == newLineInDefinition {
+			fmt.Printf("\n\t]")
+		} else if *phase != beforeFirstWord {
+			fmt.Printf("]\"\n},\n")
+		}
+
+		fmt.Printf("\n},\n{\n\t\"word\": \"%s\",", line)
+		*phase = parsingPronounciationFirstLine
+
+	} else if *phase == parsingPronounciationFirstLine {
+		fmt.Printf("\n\t"+`"pronounciation": "%s`, strings.Replace(line, `"`, `'`, -1))
+		*phase = parsingPronounciation
+	} else if *phase == parsingPronounciation {
+		fmt.Printf(" %s", line)
+	} else if *phase == parsingDefinitionFirstLine {
+		fmt.Printf(
+			"\n\t\"definition\": [\n\t\"%s",
+			strings.Replace(line, `"`, `'`, -1))
+
+		*phase = parsingDefinition
+
+	} else if *phase == parsingDefinition || *phase == newLineInDefinition {
+
+		if *phase == newLineInDefinition {
+			fmt.Printf(",\n\t\"")
+			*phase = parsingDefinition
+		}
+
+		fmt.Printf("%s", strings.Replace(line, `"`, `'`, -1))
+	}
+	return 1
 }
